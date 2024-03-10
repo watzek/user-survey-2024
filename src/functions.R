b99fc542ad486c8869767dc35d5e253ae1107a1a
Jeremy McWilliams <jeremym@lclark.edu>
2024-03-10 18:09:25 +0000

    table test

---
+++ src/functions.R
@@ -0,0 +1,79 @@
+
+
+
+frequencyQuestion<-function(data, columnName, label, fileName, casOnly=FALSE, gradOnly=FALSE){
+  
+  #remove na rows for selected column:
+  dataNaRemoved<-data %>% drop_na(columnName)
+  
+  if(casOnly==TRUE){
+    dataNaRemoved<- dataNaRemoved %>%
+      filter(campus=="CAS")
+  }
+  if(gradOnly==TRUE){
+    dataNaRemoved<- dataNaRemoved %>%
+      filter(campus=="GRAD")
+  } 
+  
+  total<-nrow(dataNaRemoved)
+  dataNaRemoved
+  
+  all<-dataNaRemoved %>%
+    group_by(!!sym(columnName)) %>%
+    summarize(count=n(), percent=round(n()/total*100, digits=1)) %>%
+    arrange(match(!!sym(columnName), c("Daily", "Weekly", "Monthly", "Once a semester", "Never")))
+  
+  # turn this into factors, so it preserves the order on the charts
+  all[[columnName]] <- factor(all[[columnName]], levels = all[[columnName]])
+  #return (all)
+  
+  chart<-all %>% 
+    ggplot(mapping=aes(x=.data[[columnName]], y=percent, fill=.data[[columnName]]))+
+    geom_bar(stat="identity")+
+    geom_text(aes(label = paste(percent, "%")), vjust = -0.5, size = 3) + 
+    labs(title=paste("Frequency of Watzek Visits for",label), y="Percent of respondants", x="", fill=paste("Frequency"), caption=paste("n=", total, sep = ""))
+  
+  print(chart)
+  ggsave(chart, file=paste("../output/",fileName))
+  
+  
+}
+
+
+needsQuestion<-function(data, columnName, label, fileName, casOnly=FALSE, gradOnly=FALSE){
+  
+  
+  dataNaRemoved<-data %>% drop_na(columnName)
+
+  dataNaRemoved
+  if(casOnly==TRUE){
+    dataNaRemoved<- dataNaRemoved %>%
+      filter(campus=="CAS")
+  }
+  if(gradOnly==TRUE){
+    dataNaRemoved<- dataNaRemoved %>%
+      filter(campus=="GRAD")
+  } 
+  total<-nrow(dataNaRemoved)
+  
+  all<-dataNaRemoved %>%
+    group_by(!!sym(columnName)) %>%
+    summarize(count=n(), percent=round(n()/total*100, digits=1)) %>%
+    arrange(match(!!sym(columnName), c("Meets my needs", "Does not meet my needs", "I don't have a need for this", "I'm not familiar with this")))
+  
+  all[[columnName]] <- factor(all[[columnName]], levels = all[[columnName]])
+  #return (all)
+  
+  chart<-all %>% 
+    ggplot(mapping=aes(x=.data[[columnName]], y=percent, fill=.data[[columnName]]))+
+    geom_bar(stat="identity")+
+    geom_text(aes(label = paste(percent, "%")), vjust = -0.5, size = 3) +
+    theme(axis.text.x=element_blank())+
+    labs(title=paste("Meets needs for",label), y="Percent of respondants", x="", fill=paste("Frequency"), caption=paste("n=", total, sep = ""))
+  
+  print(chart)
+  ggsave(chart, file=paste("../output/",fileName))
+  
+}
+
+
